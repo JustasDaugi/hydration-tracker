a40ba57721b9b0c519343fe7fa28268bd20e0fc6
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-09-06 19:27:06 +0300

    feat: add event handler for OK button

--- src/views/WelcomeView/WelcomeView.vue
+++ src/views/WelcomeView/WelcomeView.vue
@@ -1,11 +1,20 @@
 <script setup>
+import { inject } from 'vue';
 import HeaderText from './components/HeaderText.vue';
 import BaseRadioButtons from '../../components/base/BaseRadioButtons.vue';
 import BaseWeightInput from '../../components/base/BaseWeightInput.vue';
 import BaseClockPicker from '../../components/base/BaseClockPicker.vue';
+import OKButton from './components/OKButton.vue';
 import { baseStorage } from '../../composables';
 
 const { selectedUnit, weight, wakeUpTime, sleepTime } = baseStorage();
+
+const toggleViews = inject('toggleViews');
+
+const handleOKClick = () => {
+  console.log('OK button clicked');
+  toggleViews();
+}
 </script>
 
 <template>
@@ -17,11 +26,21 @@ const { selectedUnit, weight, wakeUpTime, sleepTime } = baseStorage();
       v-model:wakeUpTime="wakeUpTime" 
       v-model:sleepTime="sleepTime" 
     />
+    <div class="button-container">
+      <OKButton @click="handleOKClick" />
+    </div>
   </main>
 </template>
 
 <style scoped>
 .welcome-view {
   padding-top: 120px;
+  position: relative;
+}
+
+.button-container {
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
 }
 </style>
