61ada7218c4123f344375df43bad64a98993457b
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-09-06 19:32:43 +0300

    feat: add refs for controlling WelcomeView visibility

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { ref, provide } from 'vue';
+import { RouterLink, RouterView } from 'vue-router';
+import HeaderMenu from './layouts/HeaderMenu.vue';
+
+const showHeader = ref(false);
+const showWelcomeView = ref(true);
+
+provide('showHeader', showHeader);
+provide('showWelcomeView', showWelcomeView);
+
+const toggleViews = () => {
+  showHeader.value = true;
+  showWelcomeView.value = false;
+};
+
+provide('toggleViews', toggleViews);
 </script>
 
 <template>
-  <header>
-
-
-      <nav>
-
-      </nav>
-  </header>
-
-  <RouterView />
+  <HeaderMenu v-if="showHeader" />
+  <RouterView v-if="showWelcomeView" />
 </template>
 
 <style scoped>
