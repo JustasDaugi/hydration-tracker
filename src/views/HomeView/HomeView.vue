e628f0e34be4185106c43e5972be0c7ae4cf7ef0
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-09-07 12:23:22 +0300

    feat: import DrinkTarget component to HomeView

---
+++ src/views/HomeView/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { inject } from 'vue';
+import DrinkTarget from './components/DrinkTarget.vue';
+
+const showWelcomeView = inject('showWelcomeView');
+</script>
+
+<template>
+  <div v-if="!showWelcomeView" class="home-view">
+    <DrinkTarget />
+
+  </div>
+</template>
+
+<style scoped>
+.home-view {
+  padding-top: 60px;
+}
+</style>
