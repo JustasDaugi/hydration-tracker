87d1653b5ec14d0301609d917466f089b5b36b3d
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-09-08 17:19:53 +0300

    feat: use local storage to save the selected cup size

--- src/composables/index.js
+++ src/composables/index.js
@@ -5,11 +5,13 @@ export function baseStorage() {
   const weight = useStorage('weight', '');
   const wakeUpTime = useStorage('wakeUpTime', '09:00');
   const sleepTime = useStorage('sleepTime', '21:00');
+  const selectedCupSize = useStorage('selectedCupSize', 200);
 
   return {
     selectedUnit,
     weight,
     wakeUpTime,
-    sleepTime
+    sleepTime,
+    selectedCupSize
   };
 }
