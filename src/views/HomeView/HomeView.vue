a8fa5a05007dc18d855d00af9e5ef618f800ea11
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-09-09 19:28:33 +0300

    styles: add wrappers for containers to fix positioning issues

--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { inject } from 'vue'
 import DrinkTarget from './components/DrinkTarget.vue'
+import TodaysRecords from './components/TodaysRecords.vue'
 import SwitchCup from './components/SwitchCup.vue'
 
 const showWelcomeView = inject('showWelcomeView')
@@ -8,8 +9,13 @@ const showWelcomeView = inject('showWelcomeView')
 
 <template>
   <div v-if="!showWelcomeView" class="home-view">
-    <DrinkTarget />
-    <SwitchCup />
+    <div class="drink-target-wrapper">
+      <DrinkTarget />
+      <div class="switch-cup-wrapper">
+        <SwitchCup />
+      </div>
+    </div>
+    <TodaysRecords class="todays-records-wrapper" />
   </div>
 </template>
 
@@ -17,5 +23,35 @@ const showWelcomeView = inject('showWelcomeView')
 .home-view {
   padding-top: 60px;
   position: relative;
+  min-height: 100vh;
+}
+
+.drink-target-wrapper {
+  position: fixed;
+  inset: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  transform: translateY(-10%); /* Move the entire wrapper upwards */
+}
+
+.switch-cup-wrapper {
+  position: absolute;
+  bottom: 220px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.todays-records-wrapper {
+  position: fixed;
+  bottom: 30px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 300px;
+  max-width: 90%;
 }
 </style>
