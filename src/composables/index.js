b96b1d58aee1549c207cd064223712be0e07abe3
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-09-10 17:20:43 +0300

    feat: new values added for saving intake and todays records in localStorage

--- src/composables/index.js
+++ src/composables/index.js
@@ -6,12 +6,16 @@ export function baseStorage() {
   const wakeUpTime = useStorage('wakeUpTime', '09:00');
   const sleepTime = useStorage('sleepTime', '21:00');
   const selectedCupSize = useStorage('selectedCupSize', 200);
+  const currentIntake = useStorage('currentIntake', 0);
+  const lastResetDate = useStorage('lastResetDate', '');
 
   return {
     selectedUnit,
     weight,
     wakeUpTime,
     sleepTime,
-    selectedCupSize
+    selectedCupSize,
+    currentIntake,
+    lastResetDate
   };
 }
