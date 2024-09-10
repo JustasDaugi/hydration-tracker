dfd9c295ee5074dbc580ec9a10fcbdabff44cf77
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-09-10 17:21:42 +0300

    feat: import vuetify and dependencies, create instance of vuetify

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap'
 
 import App from './App.vue'
 import router from './router'
@@ -10,7 +18,51 @@ import 'vue-datepicker-next/index.css'
 
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia()
+app.use(pinia)
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
+app.use(vuetify)
+
 app.use(router)
 app.use(Datepicker)
+
 app.mount('#app')
+
+
+
+
+// import './assets/main.css'
+
+// import { createApp } from 'vue'
+// import { createPinia } from 'pinia'
+// import { createVuetify } from 'vuetify'
+// import 'vuetify/styles'
+
+// import App from './App.vue'
+// import router from './router'
+// import Datepicker from 'vue-datepicker-next'
+// import 'vue-datepicker-next/index.css'
+
+// const app = createApp(App)
+
+// const pinia = createPinia()
+// app.use(pinia)
+
+// const vuetify = createVuetify()
+// app.use(vuetify)
+
+// app.use(router)
+// app.use(Datepicker)
+
+// app.mount('#app')
