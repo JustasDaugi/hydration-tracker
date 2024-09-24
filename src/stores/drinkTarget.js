832b8b3325f4dcefb60ccdf0bc6f462f675abdfb
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-09-24 18:14:48 +0300

    fix: remove functions related to calculating intake for imperial system

--- src/stores/drinkTarget.js
+++ src/stores/drinkTarget.js
@@ -3,43 +3,23 @@ import { baseStorage } from '../composables'
 
 const ML_PER_KG = 31.7
 
-function convertKgToLbs(kg) {
-  return kg * 2.20462
-}
-
-function convertLbsToKg(lbs) {
-  return lbs / 2.20462
-}
-
 function calculateWaterIntakeInMl(weightInKg) {
   return Math.round(weightInKg * ML_PER_KG)
 }
 
-function convertMlToOz(ml) {
-  return ml / 29.5735
-}
-
 export function calculateWaterIntake() {
-  const { weight, selectedUnit } = baseStorage()
+  const { weight } = baseStorage()
 
   const weightInKg = computed(() => {
-    const weightValue = parseFloat(weight.value || '0')
-    return selectedUnit.value === 'imperial' ? convertLbsToKg(weightValue) : weightValue
+    return parseFloat(weight.value || '0')
   })
 
   const recommendedIntakeInMl = computed(() => {
     return calculateWaterIntakeInMl(weightInKg.value)
   })
 
-  const recommendedIntakeInOz = computed(() => {
-    return Math.round(convertMlToOz(recommendedIntakeInMl.value))
-  })
-
-  const weightInLbs = computed(() => convertKgToLbs(weightInKg.value))
-
   return {
     recommendedIntakeMl: recommendedIntakeInMl,
-    recommendedIntakeOz: recommendedIntakeInOz,
-    weightInLbs
+    weightInKg
   }
 }
