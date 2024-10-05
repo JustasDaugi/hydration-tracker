45041e27c07538c8492a42a4789d564b29a7713f
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-10-05 16:20:38 +0300

    fix: add root dir to createRouter for deployment

--- src/router/index.js
+++ src/router/index.js
@@ -2,25 +2,27 @@ import { createRouter, createWebHistory } from 'vue-router'
 import WelcomeView from '../views/WelcomeView/WelcomeView.vue'
 import SettingsView from '../views/SettingsView/SettingsView.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: WelcomeView
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: SettingsView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/WelcomeView/WelcomeView.vue')
+  },
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: WelcomeView
-    },
-    {
-      path: '/settings',
-      name: 'settings',
-      component: SettingsView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/WelcomeView/WelcomeView.vue')
-    }
-  ]
+  history: createWebHistory('/hydro-meter/'),
+  routes
 })
 
 export default router
