14d23023a8410b78d8046c5d3a47b3f897f690b9
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-10-05 15:35:11 +0300

    feat: add routing to settings view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WelcomeView from '../views/WelcomeView/WelcomeView.vue'
-
+import SettingsView from '../views/SettingsView/SettingsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,11 +11,13 @@ const router = createRouter({
       component: WelcomeView
     },
     {
+      path: '/settings',
+      name: 'settings',
+      component: SettingsView
+    },
+    {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/WelcomeView/WelcomeView.vue')
     }
   ]
