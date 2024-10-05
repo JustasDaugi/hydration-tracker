12b906b6458b85e52becf7091312d2be129bdeae
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-10-05 15:45:34 +0300

    feat: add check if user visited Welcome page, show HomeView if set to true

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-import { ref, provide } from 'vue';
-import { RouterView } from 'vue-router';
+import { ref, provide, watch } from 'vue';
+import { RouterView, useRouter } from 'vue-router';
 import HeaderMenu from './layouts/HeaderMenu.vue';
 import HomeView from './views/HomeView/HomeView.vue';
 
+const router = useRouter();
 const showHeader = ref(false);
 const showWelcomeView = ref(true);
 
@@ -13,17 +14,22 @@ provide('showWelcomeView', showWelcomeView);
 const toggleViews = () => {
   showHeader.value = true;
   showWelcomeView.value = false;
+  localStorage.setItem('hasVisited', 'true');
 };
 
 provide('toggleViews', toggleViews);
+
+watch(() => router.currentRoute.value, (newRoute) => {
+  const hasVisited = localStorage.getItem('hasVisited');
+  if (hasVisited || newRoute.path === '/settings') {
+    showHeader.value = true;
+    showWelcomeView.value = false;
+  }
+}, { immediate: true });
 </script>
 
 <template>
   <HeaderMenu v-if="showHeader" />
-  <RouterView v-if="showWelcomeView" />
-  <HomeView v-if="!showWelcomeView" />
+  <RouterView v-if="showWelcomeView || $route.path === '/settings'" />
+  <HomeView v-if="!showWelcomeView && $route.path !== '/settings'" />
 </template>
-
-<style scoped>
-
-</style>
