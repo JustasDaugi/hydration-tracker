b23dc1233c0f168f1ea46a9ec45aca24a4a011ae
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-10-05 15:25:55 +0300

    style: add wrappers, use bootstrap  for components, css and media queries for style section

--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -5,60 +5,215 @@ import TodaysRecords from './components/TodaysRecords.vue'
 import SwitchCup from './components/SwitchCup.vue'
 
 const showWelcomeView = inject('showWelcomeView')
+
 </script>
 
 <template>
-  <div v-if="!showWelcomeView" class="home-view">
-    <div class="drink-target-wrapper">
-      <DrinkTarget />
-      <div class="switch-cup-wrapper">
-        <SwitchCup />
+  <div v-if="!showWelcomeView">
+    <div class="row justify-content-center drink-target-row">
+      <div class="drink-target-wrapper">
+        <DrinkTarget />
       </div>
     </div>
-    <div class="todays-records-container">
-      <TodaysRecords class="todays-records-wrapper" />
+
+    <div class="row justify-content-center">
+      <div class="col-12 col-sm-10 col-md-8 col-lg-6">
+        <div class="switch-cup-wrapper mb-3">
+          <SwitchCup />
+        </div>
+      </div>
+    </div>
+
+    <div class="row justify-content-center mb-3">
+      <div class="col-12 col-sm-10 col-md-8 col-lg-6">
+        <div class="todays-records-wrapper">
+          <TodaysRecords />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
+
 <style scoped>
-.home-view {
-  padding-top: 60px;
-  position: relative;
-  min-height: 100vh;
+.drink-target-row {
+  height: 40vh;
 }
 
 .drink-target-wrapper {
-  position: fixed;
-  inset: 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  transform: translateY(-10%);
+  width: 80vw;
+  height: 50vw;
+  max-width: 380px;
+  max-height: 275px;
+  margin-top: 60px;
+}
+
+
+.switch-cup-wrapper,
+.todays-records-wrapper {
+  width: 100%;
 }
 
 .switch-cup-wrapper {
-  position: absolute;
-  bottom: 220px;
-  left: 50%;
-  transform: translateX(-50%);
-  width: 100%;
-  display: flex;
-  justify-content: center;
+  overflow-x: auto;
+  padding: 10px 0;
+  max-width: 420px;
+  margin: 0 auto;
 }
 
-.todays-records-container {
-  position: fixed;
-  bottom: -10px;
-  left: 0;
-  right: 0;
-  display: flex;
-  justify-content: center;
+@media (min-width: 576px) {
+  .drink-target-wrapper {
+    width: 60vw;
+    height: 60vw;
+  }
+
+  .switch-cup-wrapper {
+    overflow-x: auto;
+    padding: 10px 0;
+    max-width: 420px;
+    margin: 0 auto;
+  }
 }
 
-.todays-records-wrapper {
-  width: 500px;
-  max-width: 90%;
+@media (min-width: 768px) {
+  .drink-target-wrapper {
+    width: 50vw;
+    height: 50vw;
+  }
+
+  .switch-cup-wrapper {
+    overflow-x: auto;
+    padding: 10px 0;
+    max-width: 420px;
+    margin: 0 auto;
+  }
+}
+
+@media (min-width: 768px) and (min-height: 840px) {
+  .drink-target-wrapper {
+    margin-top: 20px;
+  }
+
+  .switch-cup-wrapper {
+    margin-top: 20px;
+    overflow-x: auto;
+    padding: 10px 0;
+    max-width: 420px;
+    margin: 0 auto;
+  }
+}
+
+@media (min-width: 992px) {
+  .drink-target-wrapper {
+    width: 380px;
+    height: 380px;
+  }
+
+  .switch-cup-wrapper {
+    overflow-x: auto;
+    padding: 10px 0;
+    max-width: 420px;
+    margin: 0 auto;
+  }
+}
+
+@media (max-width: 477px) and (max-height: 840px) {
+  .drink-target-wrapper {
+    width: 70vw;
+    height: 70vw;
+  }
+
+  .switch-cup-wrapper {
+    overflow-x: auto;
+    padding: 10px 0;
+    max-width: 420px;
+    margin: 0 auto;
+  }
+}
+
+@media (min-width: 1024px) and (min-height: 840px) {
+  .drink-target-wrapper,
+  .switch-cup-wrapper,
+  .todays-records-wrapper {
+    position: fixed;
+    left: 50%;
+    transform: translateX(-50%);
+    max-width: 380px;
+  }
+
+  .drink-target-wrapper {
+    top: 7%;
+  }
+
+  .switch-cup-wrapper {
+    top: 50%;
+    overflow-x: auto;
+    padding: 10px 0;
+    max-width: 420px;
+    margin: 0 auto;
+  }
+
+  .todays-records-wrapper {
+    top: 62%;
+  }
+
+  .container-lg {
+    justify-content: center;
+    align-items: center;
+  }
+}
+
+@media (width: 320px) and (height: 840px) {
+  .drink-target-wrapper {
+    margin-top: 115px;
+    height: 170px;
+    
+  }
+
+  .switch-cup-wrapper {
+    overflow-x: auto;
+    padding: 10px 0;
+    max-width: 420px;
+    margin: 0 auto;
+  }
+}
+
+@media (width: 375px) and (height: 840px) {
+  .drink-target-wrapper {
+    margin-top: 85px;
+  }
+
+  .switch-cup-wrapper {
+    overflow-x: auto;
+    padding: 10px 0;
+    max-width: 420px;
+    margin: 0 auto;
+  }
+}
+
+@media (width: 425px) and (height: 840px) {
+  .drink-target-wrapper {
+    margin-top: 50px;
+  }
+
+  .switch-cup-wrapper {
+    overflow-x: auto;
+    padding: 10px 0;
+    max-width: 420px;
+    margin: 0 auto;
+  }
+}
+
+@media (width: 768px) and (height: 840px) {
+  .drink-target-wrapper {
+    margin-top: 45px;
+  }
+
+  .switch-cup-wrapper {
+    overflow-x: auto;
+    padding: 10px 0;
+    max-width: 420px;
+    margin: 0 auto;
+  }
 }
 </style>
